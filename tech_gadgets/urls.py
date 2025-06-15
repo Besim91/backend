0493c1852ab5e2bb94a8602ec4b5bf71062a418c
Besim91 <78695114+Besim91@users.noreply.github.com>
2025-06-15 10:50:39 +0200

    Get and Post view together

--- tech_gadgets/urls.py
+++ tech_gadgets/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .views import start_page_view, single_gadgeds_view, single_gadgeds_slug_view, \
-        single_gadgeds_post_view
+from .views import start_page_view, single_gadgeds_int_view, single_gadgeds_view
 
 
 urlpatterns = [
     path('', start_page_view),
-    path('gadget/<int:gadget_id>/', single_gadgeds_view),
-    path('gadget/<slug:gadget_slug>/', single_gadgeds_slug_view, name="gadget_slug_url"),
-    path('gadget/send_gadget/', single_gadgeds_post_view)
+    path('gadget/', single_gadgeds_view),
+    path('gadget/<int:gadget_id>/', single_gadgeds_int_view),
+    path('gadget/<slug:gadget_slug>/', single_gadgeds_view, name="gadget_slug_url")
 ]
--- tech_gadgets/views.py
+++ tech_gadgets/views.py
@@ -5,30 +5,35 @@ import json
 from django.utils.text import slugify
 from django.urls import reverse
 
+
+
 def start_page_view(request):
     return HttpResponse("Das hat gut geklappt!")
 
-def single_gadgeds_view(request, gadget_id):
+
+
+def single_gadgeds_int_view(request, gadget_id):
     if len(gadgets) > gadget_id:
         new_slug = slugify(gadgets[gadget_id]["name"])
         new_url = reverse("gadget_slug_url", args=[new_slug])
         return redirect(new_url)
     return HttpResponse("not found")
 
-def single_gadgeds_slug_view(request, gadget_slug):
-    gadget_match = {"result": "nothing Besim"}
+
+
+def single_gadgeds_view(request, gadget_slug=""):
     
-    for gadget in gadgets:
-        if slugify(gadget["name"]) == gadget_slug:
-            gadget_match = gadget
-            
-            
-    if gadget_match:        
-        return JsonResponse(gadget_match)
-    raise Http404()
+    if request.method == "GET":
+        gadget_match = None
+        for gadget in gadgets:
+            if slugify(gadget["name"]) == gadget_slug:
+                gadget_match = gadget
+                
+        if gadget_match:        
+            return JsonResponse(gadget_match)
+        raise Http404()
 
-
-def single_gadgeds_post_view(request):
+    
     if request.method == "POST":
         try:
             data = json.loads(request.body)
@@ -36,3 +41,4 @@ def single_gadgeds_post_view(request):
             return JsonResponse({"response": "Das wahr was"})        
         except:
             return JsonResponse({"response": "Das wahr wohl nix"})
+
